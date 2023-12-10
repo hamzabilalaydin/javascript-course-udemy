51e28a99b7745e412231ee4ec2e1a3a5b2901b94
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-10 17:55:51 +0300

    core game logic implemented win and draw situations sorted

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,21 +1,65 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-//                              // you can store functions in variables
-// const start = function () {
-//   console.log("Game is starting...");
-// };
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WIN = "PLAYER WINS";
+const RESULT_COMPUTER_WIN = "COMPUTER WINS";
 
-// const person = {
-//   name: "hamza",
-//   greet: function greet() {
-//     console.log("Hello there!");
-//   },
-// };
-// person.greet();
+let gameIsRunning = false;
 
-// console.dir(startGame); // FUNCTIONS ARE OBJECTS
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
+  ) {
+    alert(`Invalid choice! We choose ${ROCK} for you`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+const getComputerChoice = function () {
+  const randomVAlue = Math.random();
+  if (randomVAlue < 0.34) {
+    return ROCK;
+  } else if (randomVAlue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WIN;
+  } else {
+    return RESULT_COMPUTER_WIN;
+  }
+};
 
 startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   // we can give anonymous functions' a name
   console.log("Game is starting..."); // if we need a function just one time, we can use it anonymously
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
