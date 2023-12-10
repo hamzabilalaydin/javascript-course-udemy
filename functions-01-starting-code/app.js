28f6063faf02f0306b9d4615c8257a61e6bc8ad8
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-10 16:24:53 +0300

    functions basically are objects

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,17 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("Game is starting...");
+}
+
+// const person = {
+//   name: "hamza",
+//   greet: function greet() {
+//     console.log("Hello there!");
+//   },
+// };
+// person.greet();
+
+// console.dir(startGame); // FUNCTIONS ARE OBJECTS
+
+startGameBtn.addEventListener("click", startGame);
