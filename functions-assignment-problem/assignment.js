295ab85cfe83cbb42d491ed1fc518f7311da30a1
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-11 00:19:05 +0300

    function assignment submitted

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,40 @@
-function sayHello(name) {
-  console.log("Hi " + name);
+const sayHello = (name) => console.log("Hi " + name);
+
+const sayHello2 = (name, phrase) => console.log(phrase + " " + name);
+
+const sayHello3 = () => console.log("Hi asdasd");
+const sayHello4 = (name) => "Hi " + name;
+
+sayHello("hba");
+sayHello2("hba", "hello");
+sayHello3();
+console.log(sayHello4("hba"));
+
+const sayHello5 = (name, phrase = "hi") =>
+  console.log(phrase + " " + name);
+
+sayHello5("hba");
+sayHello5("hba", "hihi");
+
+function checkInput(cb, ...args) {
+  let hasEmptyText = false;
+  for (const text of args) {
+    if (!text) {
+      hasEmptyText = true;
+      break;
+    }
+  }
+  if (!hasEmptyText) {
+    cb();
+  }
 }
 
-sayHello();
+checkInput(
+  () => {
+    console.log("All not empty");
+  },
+  "Hello",
+  "12",
+  "adsas",
+  "Not empty"
+);
