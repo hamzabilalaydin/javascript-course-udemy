c5db2185d146476cf63235ec36981ff4e7691f39
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-11 23:42:10 +0300

    dom assignment-1 submitted

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,14 @@
+const li1 = document.getElementById("task-1");
+const li11 = document.querySelector("li:first-of-type");
+
+li1.style.color = "white";
+li1.style.backgroundColor = "black";
+
+const task2 = document.querySelector("title");
+task2.textContent = "Assignment - Solved!";
+
+const task21 = document.head.querySelector("title");
+task21.textContent = "Assignment - Solved!";
+
+const task3 = document.querySelector("h1");
+task3.textContent = "Assignment - Solved!";
