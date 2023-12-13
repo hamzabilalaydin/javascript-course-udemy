b25c94ac154edeb2a76773516ffa6e5e03b422c0
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-13 15:15:40 +0300

    how to create arrays

---
+++ arrays-01-starting-code/app.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = new Array(1, 4); //default: [] if we pass more than one number that would be its items
+// const moreNumbers = Array(5); //default: []  if we pass just one number that would be it length
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
+
+const listItems = document.querySelectorAll("li");
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(moreNumbers);
