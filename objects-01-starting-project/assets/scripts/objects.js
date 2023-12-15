f111ed10ed560326f486ec4b677130457376081e
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-15 17:34:22 +0300

    rendering elements and dynamic properties

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,25 +1,81 @@
-const movieList = document.getElementById("movie-list");
-movieList.style["background-color"] = "red";
-movieList.style.display = "block";
+// const movieList = document.getElementById("movie-list");
+// movieList.style["background-color"] = "red";
+// movieList.style.display = "block";
 
-const userKeyName = "level";
+// const userKeyName = "level";
 
-let person = {
-  name: "hba",
-  age: 28,
-  hobbies: ["sports", "cooking"],
-  [userKeyName]: "...", // value of userKeyName given as a 'key'
-  greet: function () {
-    alert("Hi there!");
-  },
-  1.5: "hello", // numbers can be used as keys
+// let person = {
+//   name: "hba",
+//   age: 28,
+//   hobbies: ["sports", "cooking"],
+//   [userKeyName]: "...", // value of userKeyName given as a 'key'
+//   greet: function () {
+//     alert("Hi there!");
+//   },
+//   1.5: "hello", // numbers can be used as keys
+// };
+
+// delete person.age;
+// person.isAdmin = true;
+// // person.greet();
+// const keyName = "name";
+
+// console.log(person[keyName]);
+// console.log(person[1.5]); // with numbers we don't have to use "quotes"
+// console.log(person);
+
+const addMovieBtn = document.querySelector("#add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movies = [];
+
+const renderMovies = () => {
+  const movielist = document.getElementById("movie-list");
+
+  if (movies.length === 0) {
+    movielist.classList.remove("visible");
+    return;
+  } else {
+    movielist.classList.add("visible");
+  }
+  movielist.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    let text = movie.info.title + " - ";
+    for (const key in movie.info) {
+      if (key !== "title") {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movielist.append(movieEl);
+  });
 };
 
-delete person.age;
-person.isAdmin = true;
-// person.greet();
-const keyName = "name";
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
 
-console.log(person[keyName]);
-console.log(person[1.5]); // with numbers we don't have to use "quotes"
-console.log(person);
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title: title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+
+  movies.push(newMovie);
+  renderMovies();
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
