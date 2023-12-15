1eeec3ba642fd3fcdd9c4b6c9dc626f634598fd3
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-15 14:57:54 +0300

    property access and property order

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -2,16 +2,24 @@ const movieList = document.getElementById("movie-list");
 movieList.style["background-color"] = "red";
 movieList.style.display = "block";
 
+const userKeyName = "level";
+
 let person = {
   name: "hba",
   age: 28,
   hobbies: ["sports", "cooking"],
+  [userKeyName]: "...", // value of userKeyName given as a 'key'
   greet: function () {
     alert("Hi there!");
   },
+  1.5: "hello", // numbers can be used as keys
 };
 
 delete person.age;
 person.isAdmin = true;
 // person.greet();
-console.log(person["name"]);
+const keyName = "name";
+
+console.log(person[keyName]);
+console.log(person[1.5]); // with numbers we don't have to use "quotes"
+console.log(person);
