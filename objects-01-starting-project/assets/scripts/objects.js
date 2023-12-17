05dcd87dceb253f2a22f9712ec156eee5d0dec49
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-17 23:20:50 +0300

    get and set

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -47,13 +47,13 @@ const renderMovies = (filter = "") => {
   filteredMovies.forEach((movie) => {
     const movieEl = document.createElement("li");
     const { info, ...otherProps } = movie;
-    console.log(otherProps);
+    // console.log(otherProps);
     // const { title: movieTitle } = info;
     let { getFormattedTitle } = movie;
     // getFormattedTitle = getFormattedTitle.bind(movie);
     let text = getFormattedTitle.apply(movie) + " - ";
     for (const key in info) {
-      if (key !== "title") {
+      if (key !== "title" && key !== "_title") {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -67,25 +67,33 @@ const addMovieHandler = () => {
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
 
-  if (
-    title.trim() === "" ||
-    extraName.trim() === "" ||
-    extraValue.trim() === ""
-  ) {
+  if (extraName.trim() === "" || extraValue.trim() === "") {
     return;
   }
 
   const newMovie = {
     info: {
-      title: title,
+      set title(val) {
+        if (val.trim() === "") {
+          this._title = "DEFAULT";
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
-    getFormattedTitle: () => {
+    getFormattedTitle() {
       console.log(this);
-      return this.info.title.toUpperCase();
+      return this.info.title;
     },
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();
