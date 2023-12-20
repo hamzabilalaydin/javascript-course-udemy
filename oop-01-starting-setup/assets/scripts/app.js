8af4bcfc5360a0f24c4b76a6d0c8bca0b4e87535
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-20 10:19:08 +0300

    Connecting classes

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,9 @@
 class Product {
-  title = "DEFAULT";
-  imgUrl;
-  description;
-  price;
+  // title = "DEFAULT";
+  // imgUrl;
+  // description;
+  // price;
+  //IF WE DEFINING OUR CLASS IN CONSTRUCTOR WE DON'T NEED THESE
 
   constructor(title, image, desc, price) {
     this.title = title;
@@ -12,8 +13,31 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imgUrl}" alt="${this.product.title}"/>
+        <div class = 'product-item__content'>
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to cart</button>
+        </div>
+      </div>
+      `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A pillow",
       "https://bpb-us-w2.wpmucdn.com/u.osu.edu/dist/4/4139/files/2016/01/pillows-1z38kzh.jpg",
@@ -26,29 +50,22 @@ const productList = {
       "A carpet which you might like - or not.",
       89.99
     ),
-  ],
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-      <div>
-        <img src="${prod.imgUrl}" alt="${prod.title}"/>
-        <div class = 'product-item__content'>
-        <h2>${prod.title}</h2>
-        <h3>\$${prod.price}</h3>
-        <p>${prod.description}</p>
-        <button>Add to cart</button>
-        </div>
-      </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
