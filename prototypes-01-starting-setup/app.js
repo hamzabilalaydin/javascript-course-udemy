f4da8e084bf7eaa6242a5c012bc5e22579de1561
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-20 23:57:33 +0300

    Setting and Getting prototypes

--- prototypes-01-starting-setup/app.js
+++ prototypes-01-starting-setup/app.js
@@ -51,10 +51,52 @@ class Person {
 // console.log(person);
 // console.dir(Object.prototype.__proto__);
 
-const person1 = new Person();
-const person2 = new Person();
-person1.greet();
-console.log(person1);
+// const person1 = new Person();
+// const person2 = new Person();
+// person1.greet();
+// console.log(person1);
 
-const button = document.getElementById("btn");
-button.addEventListener("click", person1.greet.bind(person1));
+// const button = document.getElementById("btn");
+// button.addEventListener("click", person1.greet.bind(person1));
+
+const course = {
+  title: "Javascript the complete guide",
+  rating: 4,
+};
+
+// console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating() {
+    console.log(`${this.rating}`);
+  },
+});
+
+const student = Object.create(
+  {
+    printProgress: function () {
+      console.log(this.progress);
+    },
+  },
+  {
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: "Hba",
+      writable: false,
+    },
+  }
+);
+
+// student.name = "Hba";
+
+Object.defineProperty(student, "progress", {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false,
+});
+
+student.printProgress();
+console.log(student);
+course.printRating();
