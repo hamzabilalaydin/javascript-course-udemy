0556635a996de86fbe3b52a87960828c9273c9a6
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-21 13:37:02 +0300

    Loading scripts dynamically

--- practice-oop-01-starting-setup/assets/scripts/app.js
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -185,6 +185,16 @@ class App {
     finishedProjecstList.setSwtichHandlerFunction(
       activeProjecstList.addProject.bind(activeProjecstList)
     );
+
+    document
+      .getElementById("start-analytics-btn")
+      .addEventListener("click", this.startAnalytics);
+  }
+  static startAnalytics() {
+    const analyticsScript = document.createElement("script");
+    analyticsScript.src = "assets/scripts/analytics.js";
+    analyticsScript.defer = true;
+    document.head.append(analyticsScript);
   }
 }
 
