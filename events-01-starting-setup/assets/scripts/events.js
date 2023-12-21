442f5adfd5bd8d4781485f00a29b9a9ebcb47ff6
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-21 17:01:49 +0300

    Event handler functions & "this"

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -43,11 +43,12 @@ div.addEventListener("mouseenter", (event) => {
   console.log(event);
 });
 
-button.addEventListener("click", (event) => {
+button.addEventListener("click", function (event) {
   event.stopPropagation();
   // event.stopImmediatePropagation();
   console.log("CLICKED BUTTON");
   console.log(event);
+  console.log(this);
 });
 
 // const listItems = document.querySelectorAll("li");
@@ -58,10 +59,11 @@ button.addEventListener("click", (event) => {
 // });
 
 const list = document.querySelector("ul");
-list.addEventListener("click", (event) => {
+list.addEventListener("click", function (event) {
   // console.log(event.currentTarget);
   // event.target.classList.toggle("highlight");
   event.target.closest("li").classList.toggle("highlight");
-  form.submit();
+  // form.submit();
   button.click();
+  console.log(this);
 });
