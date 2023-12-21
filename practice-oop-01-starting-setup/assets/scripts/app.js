4c542d40c02f4fec1ad57f2fab19103333b1db4b
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-21 10:38:57 +0300

    moving dom elements and changing text contents

--- practice-oop-01-starting-setup/assets/scripts/app.js
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,21 +1,46 @@
+class DOMHelper {
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true);
+    element.replaceWith(clonedElement);
+    return clonedElement;
+  }
+  static moveElement(elementId, newDestinationSelector) {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(
+      newDestinationSelector
+    );
+    destinationElement.append(element);
+  }
+}
+
 class ToolTip {}
 
 class ProjectItem {
-  constructor(id, updateProjectListsFunction) {
+  constructor(id, updateProjectListsFunction, type) {
     this.id = id;
     this.updateProjectListsHandler = updateProjectListsFunction;
-    this.connectSwitchButton();
     this.connectMoreInfoButton();
+    this.connectSwitchButton(type);
   }
 
   connectMoreInfoButton() {}
 
-  connectSwitchButton() {
+  connectSwitchButton(type) {
     const projectItemElement = document.getElementById(this.id);
-    const swithhBtn = projectItemElement.querySelector(
+    let switchBtn = projectItemElement.querySelector(
       "button:last-of-type"
     );
-    swithhBtn.addEventListener("click", this.updateProjectListsHandler);
+    switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === "active" ? "Finish" : "Activate";
+    switchBtn.addEventListener(
+      "click",
+      this.updateProjectListsHandler.bind(null, this.id)
+    );
+  }
+
+  update(updateProjectListsFn, type) {
+    this.updateProjectListsHandler = updateProjectListsFn;
+    this.connectSwitchButton(type);
   }
 }
 
@@ -27,7 +52,11 @@ class ProjectList {
     const prjtItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjtItems) {
       this.projects.push(
-        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+        new ProjectItem(
+          prjItem.id,
+          this.switchProject.bind(this),
+          this.type
+        )
       );
     }
     console.log(this.projects);
@@ -38,8 +67,10 @@ class ProjectList {
   }
 
   // ADDING PROJECT FROM LIST TO OTHER LIST //
-  addProject() {
-    console.log(this);
+  addProject(project) {
+    this.projects.push(project);
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    project.update(this.switchProject.bind(this), this.type);
   }
 
   // REMOVING PROJECT FROM LIST IT IS CURRENTLY IN //
@@ -48,6 +79,7 @@ class ProjectList {
     //   (p) => p.id === projectId
     // );
     // this.projects.splice(projectIndex, 1);
+    console.log(this.projects.find((p) => p.id === projectId));
     this.switchHandler(this.projects.find((p) => p.id === projectId));
     this.projects = this.projects.filter((p) => p.id !== projectId);
   }
