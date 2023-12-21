ffd024a1c745bdd1a3ea7d84d3f7d183bcefc8c7
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-21 09:57:08 +0300

    Passing method references around?

--- practice-oop-01-starting-setup/assets/scripts/app.js
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,9 @@
 class ToolTip {}
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFunction) {
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectSwitchButton();
     this.connectMoreInfoButton();
   }
@@ -14,7 +15,7 @@ class ProjectItem {
     const swithhBtn = projectItemElement.querySelector(
       "button:last-of-type"
     );
-    swithhBtn.addEventListener("click");
+    swithhBtn.addEventListener("click", this.updateProjectListsHandler);
   }
 }
 
@@ -22,14 +23,24 @@ class ProjectList {
   projects = [];
 
   constructor(type) {
+    this.type = type;
     const prjtItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjtItems) {
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
     }
     console.log(this.projects);
   }
 
-  addProject() {}
+  setSwtichHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  // ADDING PROJECT FROM LIST TO OTHER LIST //
+  addProject() {
+    console.log(this);
+  }
 
   // REMOVING PROJECT FROM LIST IT IS CURRENTLY IN //
   switchProject(projectId) {
@@ -37,14 +48,21 @@ class ProjectList {
     //   (p) => p.id === projectId
     // );
     // this.projects.splice(projectIndex, 1);
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
     this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
 class App {
   static init() {
-    const activeProjectList = new ProjectList("active");
-    const finishedProjectList = new ProjectList("finished");
+    const activeProjecstList = new ProjectList("active");
+    const finishedProjecstList = new ProjectList("finished");
+    activeProjecstList.setSwtichHandlerFunction(
+      finishedProjecstList.addProject.bind(finishedProjecstList)
+    );
+    finishedProjecstList.setSwtichHandlerFunction(
+      activeProjecstList.addProject.bind(activeProjecstList)
+    );
   }
 }
 
