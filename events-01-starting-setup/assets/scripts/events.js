0cd8d3c51ed06d269557b5c4e77af60b794d22ed
hamzabilalaydin <hamzaba61@gmail.com>
2023-12-21 16:43:47 +0300

    Using event delegation

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -48,3 +48,17 @@ button.addEventListener("mouseenter", (event) => {
   console.log("CLICKED BUTTON");
   console.log(event);
 });
+
+// const listItems = document.querySelectorAll("li");
+// listItems.forEach((listItem) => {
+//   listItem.addEventListener("click", (event) => {
+//     event.target.classList.toggle("highlight");
+//   });
+// });
+
+const list = document.querySelector("ul");
+list.addEventListener("click", (event) => {
+  // console.log(event.currentTarget);
+  // event.target.classList.toggle("highlight");
+  event.target.closest("li").classList.toggle("highlight");
+});
