f1cd1c97a15792630325d4c7da3829dc064f0fd3
hamzabilalaydin <hamzaba61@gmail.com>
2024-01-29 12:21:55 +0300

    Promise error handling

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -7,7 +7,9 @@ const getPosition = (opts) => {
       (success) => {
         resolve(success);
       },
-      (error) => {},
+      (error) => {
+        reject(error);
+      },
       opts
     );
   });
@@ -29,6 +31,9 @@ function trackUserHandler() {
     .then((posData) => {
       positionData = posData;
       return setTimer(2000);
+    })
+    .catch((err) => {
+      console.log(err);
     })
     .then((data) => {
       console.log(data, positionData);
