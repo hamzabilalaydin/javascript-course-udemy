08c7189329c7a780793c4e2fe3412a559d73782c
hamzabilalaydin <hamzaba61@gmail.com>
2024-01-29 11:09:37 +0300

    asynchronous code

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,8 +1,26 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const button = document.querySelector("button");
+const output = document.querySelector("p");
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(
+    (posData) => {
+      setTimeout(() => {
+        console.log(posData);
+      }, 2000);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+  console.log("Getting position...");
 }
 
-button.addEventListener('click', trackUserHandler);
+button.addEventListener("click", trackUserHandler);
+
+// let result = 0;
+
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
+
+// console.log(result);
