a0fa7f513b0455ed999c54fa1b2eefa9f74f9f10
hamzabilalaydin <hamzaba61@gmail.com>
2024-01-31 17:14:44 +0300

    inroduction to cookies

---
+++ storage-01-starting-setup/app.js
@@ -0,0 +1,10 @@
+const storeBtn = document.getElementById("store-btn");
+const retrBtn = document.getElementById("retrieve-btn");
+
+storeBtn.addEventListener("click", () => {
+  const userId = "u123";
+  document.cookie = `uid=${userId}`;
+});
+retrBtn.addEventListener("click", () => {
+  console.log(document.cookie);
+});
