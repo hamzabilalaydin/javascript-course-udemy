673c94284c89d0b5a96f4b29b7d1719d224d3290
hamzabilalaydin <hamzaba61@gmail.com>
2024-01-31 11:35:04 +0300

    component file name changed

--- modules-01-starting-project/assets/scripts/App/Tooltip.js
+++ modules-01-starting-project/assets/scripts/App/Tooltip.js
@@ -1,4 +1,4 @@
-import { Component } from "./Component.js";
+import { Component } from "./component.js";
 
 export class Tooltip extends Component {
   constructor(closeNotifierFunction, text, hostElementId) {
