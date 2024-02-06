8aed7afde5204e210ba751f9b240c91094fda9b1
hamzabilalaydin <hamzaba61@gmail.com>
2024-02-06 15:39:03 +0300

    modal show method added

--- practice-places-01-starting-setup/src/SharePlace.js
+++ practice-places-01-starting-setup/src/SharePlace.js
@@ -1,3 +1,5 @@
+import { Modal } from "./UI/Modal.js";
+
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector("form");
@@ -12,8 +14,16 @@ class PlaceFinder {
       alert("Location feature is not available in your browser!");
       return;
     }
+
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait."
+    );
+    modal.show();
+
     navigator.geolocation.getCurrentPosition(
       (successResult) => {
+        modal.hide();
         const coordinates = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude,
@@ -21,6 +31,7 @@ class PlaceFinder {
         console.log(coordinates);
       },
       (err) => {
+        modal.hide();
         alert("Could not locate you. Please enter an adress manually!");
       }
     );
