6daed39071cc7d4f513a52181ca39f910ae70cda
hamzabilalaydin <hamzaba61@gmail.com>
2024-02-08 16:10:58 +0300

    share myplace link functionality added

--- practice-places-01-starting-setup/src/SharePlace.js
+++ practice-places-01-starting-setup/src/SharePlace.js
@@ -1,49 +1,86 @@
-import { Modal } from './UI/Modal';
-import { Map } from './UI/Map';
-import { getCoordsFromAddress } from './Utility/Location';
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+import {
+  getCoordsFromAddress,
+  getAddressFromCoords,
+} from "./Utility/Location";
 
 class PlaceFinder {
   constructor() {
-    const addressForm = document.querySelector('form');
-    const locateUserBtn = document.getElementById('locate-btn');
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+    this.shareBtn = document.getElementById("share-btn");
 
-    locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
-    addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+    locateUserBtn.addEventListener(
+      "click",
+      this.locateUserHandler.bind(this)
+    );
+    this.shareBtn.addEventListener("click", this.sharePlaceHandler);
+    addressForm.addEventListener(
+      "submit",
+      this.findAddressHandler.bind(this)
+    );
   }
 
-  selectPlace(coordinates) {
+  sharePlaceHandler() {
+    const sharedLinkInputElement = document.getElementById("share-link");
+    if (!navigator.clipboard) {
+      sharedLinkInputElement.select();
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(sharedLinkInputElement.value)
+      .then(() => {
+        alert("Copied into clipboard!");
+      })
+      .catch((err) => {
+        console.log(err);
+        sharedLinkInputElement.select();
+      });
+  }
+
+  selectPlace(coordinates, address) {
     if (this.map) {
       this.map.render(coordinates);
     } else {
       this.map = new Map(coordinates);
     }
+    this.shareBtn.disabled = false;
+    const sharedLinkInputElement = document.getElementById("share-link");
+    sharedLinkInputElement.value = `${
+      location.origin
+    }/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${
+      coordinates.lng
+    }`;
   }
 
   locateUserHandler() {
     if (!navigator.geolocation) {
       alert(
-        'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
+        "Location feature is not available in your browser - please use a more modern browser or manually enter an address."
       );
       return;
     }
     const modal = new Modal(
-      'loading-modal-content',
-      'Loading location - please wait!'
+      "loading-modal-content",
+      "Loading location - please wait!"
     );
     modal.show();
     navigator.geolocation.getCurrentPosition(
-      successResult => {
-        modal.hide();
+      async (successResult) => {
         const coordinates = {
-          lat: successResult.coords.latitude + Math.random() * 50,
-          lng: successResult.coords.longitude + Math.random() * 50
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
         };
-        this.selectPlace(coordinates);
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
       },
-      error => {
+      (error) => {
         modal.hide();
         alert(
-          'Could not locate you unfortunately. Please enter an address manually!'
+          "Could not locate you unfortunately. Please enter an address manually!"
         );
       }
     );
@@ -51,19 +88,19 @@ class PlaceFinder {
 
   async findAddressHandler(event) {
     event.preventDefault();
-    const address = event.target.querySelector('input').value;
+    const address = event.target.querySelector("input").value;
     if (!address || address.trim().length === 0) {
-      alert('Invalid address entered - please try again!');
+      alert("Invalid address entered - please try again!");
       return;
     }
     const modal = new Modal(
-      'loading-modal-content',
-      'Loading location - please wait!'
+      "loading-modal-content",
+      "Loading location - please wait!"
     );
     modal.show();
     try {
       const coordinates = await getCoordsFromAddress(address);
-      this.selectPlace(coordinates);
+      this.selectPlace(coordinates, address);
     } catch (err) {
       alert(err.message);
     }
